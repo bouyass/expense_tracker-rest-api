0be15289b04be27f16ff5587a08a81f6b7d36789
lyes makhloufi <lyes.makhloufi@outlook.fr>
2020-09-21 22:37:31 +0200

    implementing user registration

--- src/main/java/com/example/expensetrackerapi/services/UserServiceImpl.java
+++ src/main/java/com/example/expensetrackerapi/services/UserServiceImpl.java
@@ -2,10 +2,20 @@ package com.example.expensetrackerapi.services;
 
 import com.example.expensetrackerapi.exceptions.EtAuthException;
 import com.example.expensetrackerapi.model.User;
+import com.example.expensetrackerapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.regex.Pattern;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public User validateUser(String email, String password) throws EtAuthException {
         return null;
@@ -13,6 +23,14 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(String firstName, String lastName, String email, String password) throws EtAuthException {
-        return null;
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if(email != null) email = email.toLowerCase();
+        if(!pattern.matcher(email).matches())
+            throw new EtAuthException("Invalid email format");
+        Integer count = userRepository.getCountByEmail(email);
+        if(count > 0)
+            throw new EtAuthException("email already in use");
+        Integer userId = userRepository.create(firstName,lastName,email,password);
+        return userRepository.findById(userId);
     }
 }
